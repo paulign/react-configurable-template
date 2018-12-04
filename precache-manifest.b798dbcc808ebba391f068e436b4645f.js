79e63739d44f760ff1de7da6d0ab5eb61a17e720
Paul Ignatiev <vo100193ipa@gmail.com>
2018-12-04 23:38:28 +0200

    Updates

---
+++ precache-manifest.b798dbcc808ebba391f068e436b4645f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f6e442df67c68cbd701",
+    "url": "/react-configurable-template/static/js/runtime~main.3f6e442d.js"
+  },
+  {
+    "revision": "d78afa4f46e16dc07721",
+    "url": "/react-configurable-template/static/js/main.d78afa4f.chunk.js"
+  },
+  {
+    "revision": "0a5415c7f9a2fafacf9f",
+    "url": "/react-configurable-template/static/js/1.0a5415c7.chunk.js"
+  },
+  {
+    "revision": "d78afa4f46e16dc07721",
+    "url": "/react-configurable-template/static/css/main.386c46ae.chunk.css"
+  },
+  {
+    "revision": "5d07961aec26aaca261cf4f408137150",
+    "url": "/react-configurable-template/index.html"
+  }
+];
