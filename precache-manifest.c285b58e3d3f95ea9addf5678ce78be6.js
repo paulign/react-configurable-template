cfc936955860746617e5c91d0bb50d24366c0f89
Paul Ignatiev <vo100193ipa@gmail.com>
2018-12-05 08:59:37 +0200

    Updates

--- precache-manifest.c285b58e3d3f95ea9addf5678ce78be6.js
+++ precache-manifest.c285b58e3d3f95ea9addf5678ce78be6.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/react-configurable-template/static/js/runtime~main.3f6e442d.js"
   },
   {
-    "revision": "b6da73f58889e5d7b3f3",
-    "url": "/react-configurable-template/static/js/main.b6da73f5.chunk.js"
+    "revision": "94f60bee7f42bffcddad",
+    "url": "/react-configurable-template/static/js/main.94f60bee.chunk.js"
   },
   {
     "revision": "1038e382701d4854175f",
     "url": "/react-configurable-template/static/js/1.1038e382.chunk.js"
   },
   {
-    "revision": "b6da73f58889e5d7b3f3",
+    "revision": "94f60bee7f42bffcddad",
     "url": "/react-configurable-template/static/css/main.6e2c1722.chunk.css"
   },
   {
-    "revision": "eca6bc60830b10a7e6c98c33219cdef2",
+    "revision": "3777e3e18f9c3dfe360461be82ae5b91",
     "url": "/react-configurable-template/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-configurable-template/precache-manifest.6c757611f453bd7a3df0e4a7418ad099.js"
+  "/react-configurable-template/precache-manifest.c285b58e3d3f95ea9addf5678ce78be6.js"
 );
 
 workbox.clientsClaim();
